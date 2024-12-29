41fd38b32c9d6bd736baac99956413584edd3f7b
Habacuque <habacuke14@gmail.com>
2024-12-28 22:19:17 -0300

    get info reviews of a course

--- backend/apps/courses/urls.py
+++ backend/apps/courses/urls.py
@@ -4,6 +4,8 @@ from .views.views import *
 urlpatterns = [
     path('courses/', CoursesAPIView.as_view(), name='courses'),
     path('courses/<int:pk>', CourseAPIView.as_view(), name='course'),
+    path('courses/<int:course_pk>/reviews/', ReviewsAPIView.as_view(), name='course_reviews'),
+    path('courses/<int:course_pk>/reviews/<int:review_pk>', ReviewAPIView.as_view(), name='course_review'),
     path('reviews/', ReviewsAPIView.as_view(), name='reviews'),
-    path('reviews/<int:pk>', ReviewAPIView.as_view(), name='review'),
+    path('reviews/<int:review_pk>', ReviewAPIView.as_view(), name='review'),
 ]
