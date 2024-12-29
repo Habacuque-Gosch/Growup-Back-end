5a6719cc1f04c1a825e20af8c38d8acf9f521121
Habacuque <habacuke14@gmail.com>
2024-12-29 03:37:22 -0300

    types of relationship between fields/models

--- backend/apps/courses/serializers.py
+++ backend/apps/courses/serializers.py
@@ -3,12 +3,6 @@ from .models import Course, Review
 
 
 
-class CourseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Course
-        fields = '__all__'
-
 class ReviewSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -17,5 +11,29 @@ class ReviewSerializer(serializers.ModelSerializer):
         }
         model = Review
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    # NESTED RELATIONSHIP - 0-0
+    # reviews = ReviewSerializer(many=True, read_only=True)
+
+    # HYPERLINK RELATED FIELD
+    # reviews = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='review-detail')
+
+    # PRIMARYKEY RELATED FIELD
+    reviews = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = (
+            'id',
+            'title',
+            'slug',
+            'creation',
+            'update',
+            'available',
+            'reviews'
+        )
 
 
