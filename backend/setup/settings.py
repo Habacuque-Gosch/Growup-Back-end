f0223e74e8651598425cf487c1f238409b02efc7
Habacuque <habacuke14@gmail.com>
2024-12-29 03:43:30 -0300

    set pagination number global

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -127,7 +127,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
 }
 
 
