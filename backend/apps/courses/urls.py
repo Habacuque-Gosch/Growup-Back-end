68b24a5ac9bf85a89476c062c1e656d3640ccf91
Habacuque <habacuke14@gmail.com>
2024-12-28 21:55:16 -0300

    create generic views (get, post, put, delete) in routers Courses and Reviews

--- backend/apps/courses/urls.py
+++ backend/apps/courses/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views.views import *
 
 urlpatterns = [
-    path('courses/', CourseAPIView.as_view(), name='courses'),
-    path('reviews/', ReviewAPIView.as_view(), name='reviews'),
+    path('courses/', CoursesAPIView.as_view(), name='courses'),
+    path('courses/<int:pk>', CourseAPIView.as_view(), name='course'),
+    path('reviews/', ReviewsAPIView.as_view(), name='reviews'),
+    path('reviews/<int:pk>', ReviewAPIView.as_view(), name='review'),
 ]
--- backend/apps/courses/views/views.py
+++ backend/apps/courses/views/views.py
@@ -1,35 +1,28 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+from rest_framework import generics
 from apps.courses.models import Course, Review
 from apps.courses.serializers import CourseSerializer, ReviewSerializer
 
 
-class CourseAPIView(APIView):
-    def get(self, request):
-        courses = Course.objects.all()
-        serializer = CourseSerializer(courses, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class CoursesAPIView(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
-    def post(self, request):
-        serializer = CourseSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CourseAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    
+class ReviewsAPIView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
     
     
+class ReviewAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
 
-class ReviewAPIView(APIView):
-    def get(self, request):
-
-        reviews = Review.objects.all()
-        serializer = ReviewSerializer(reviews, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = ReviewSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
