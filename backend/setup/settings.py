653c367064de32b840ad4a1c509b668a7ddc2fc0
Habacuque <habacuke14@gmail.com>
2024-12-29 17:09:11 -0300

    alter permissions classes

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -121,15 +121,20 @@ CORS_ALLOWED_HEADERS = list(default_headers) + [
 ]
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated'
-    # ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.AllowAny'
+        # 'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+        # 'rest_framework.permissions.IsAdminUser'
+        # 'rest_framework.permissions.DjangoModelPermissions'
+
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 50
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
 }
 
 
