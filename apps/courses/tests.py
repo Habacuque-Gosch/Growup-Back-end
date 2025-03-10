e466f58db4131a27b439fcef7c7b63bacbe4c053
Habacuque <habacuke14@gmail.com>
2025-03-10 16:22:47 -0300

    test request courses + headers

--- apps/courses/tests.py
+++ apps/courses/tests.py
@@ -4,8 +4,12 @@ import requests
 
 # BASE API
 base_api = 'http://127.0.0.1:8000/api/v2/'
+headers = {'Authorization': 'Token c239c7a97f4a6e816c77c2af28ecb990a0b2ec0c'}
 
 # GET COURSES:
 
-courses = requests.get(base_api + 'courses/').json()
-print(courses)
+courses = requests.get(base_api + 'courses/', headers=headers)
+print(f'DATA: {courses.json()}')
+print(f'STATUS CODE: {courses.status_code}')
+
+
