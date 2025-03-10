0e1bdb77652522fec27b14c81615bcde161ae80b
Habacuque <habacuke14@gmail.com>
2025-03-10 16:16:26 -0300

    file test init

--- apps/courses/tests.py
+++ apps/courses/tests.py
@@ -1,4 +1,11 @@
-from django.test import TestCase
+# from django.test import TestCase
+import requests
 
 
+# BASE API
+base_api = 'http://127.0.0.1:8000/api/v2/'
 
+# GET COURSES:
+
+courses = requests.get(base_api + 'courses/').json()
+print(courses)
