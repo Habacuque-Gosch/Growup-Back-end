693632e53bb14310fbb991c1a0da76f15949af13
Habacuque <habacuke14@gmail.com>
2025-03-28 13:00:51 -0300

    test with jsonpath all_titles

--- apps/courses/tests.py
+++ apps/courses/tests.py
@@ -7,10 +7,12 @@ base_api = 'http://127.0.0.1:8000/api/v2/'
 headers = {'Authorization': 'Token c239c7a97f4a6e816c77c2af28ecb990a0b2ec0c'}
 
 # GET COURSES:
-
 courses = requests.get(base_api + 'courses/', headers=headers)
 # print(f'DATA: {courses.json()}')
 print(f'STATUS CODE: {courses.status_code}')
 
 results = jsonpath.jsonpath(courses.json(), 'results[0]')
-print(results) 
+print(results)
+
+all_titles = jsonpath.jsonpath(courses.json(), 'results[*].title')
+print(all_titles)
