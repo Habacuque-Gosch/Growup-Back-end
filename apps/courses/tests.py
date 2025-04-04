f269c5d762064141b2e5e837355175cdc220bc08
Habacuque <habacuke14@gmail.com>
2025-04-04 00:21:11 -0300

    apply pytest

--- apps/courses/tests.py
+++ apps/courses/tests.py
@@ -1,18 +1,11 @@
-# from django.test import TestCase
-import requests, jsonpath
+import requests
 
+class TestCourses:
+    
+    # BASE API URL
+    base_api = 'http://127.0.0.1:8000/api/v2/courses/'
+    headers = {'Authorization': 'Token c239c7a97f4a6e816c77c2af28ecb990a0b2ec0c'}
 
-# BASE API
-base_api = 'http://127.0.0.1:8000/api/v2/'
-headers = {'Authorization': 'Token c239c7a97f4a6e816c77c2af28ecb990a0b2ec0c'}
-
-# GET COURSES:
-courses = requests.get(base_api + 'courses/', headers=headers)
-# print(f'DATA: {courses.json()}')
-print(f'STATUS CODE: {courses.status_code}')
-
-results = jsonpath.jsonpath(courses.json(), 'results[0]')
-print(results)
-
-all_titles = jsonpath.jsonpath(courses.json(), 'results[*].title')
-print(all_titles)
+    def test_get_all_courses(self):
+        results = requests.get(url=self.base_api, headers=self.headers)
+        assert results.status_code == 200
