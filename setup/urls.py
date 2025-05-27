63f2dbe6e73f3842611167e4383394b42b6b6153
Habacuque <habacuke14@gmail.com>
2025-05-27 02:09:19 -0300

    include i8n

--- setup/urls.py
+++ setup/urls.py
@@ -2,20 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-# from apps.usuarios.urls import router
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from rest_framework.authtoken import views
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
-from django.shortcuts import redirect
-from django.utils.translation import get_language
 
 
 
