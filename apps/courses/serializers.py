400afecd070609b4ef5cfa1bdb24a7ae402c45f6
Habacuque Gosch <habacuke14@gmail.com>
2025-06-18 07:29:28 -0300

    exclude comments

--- apps/courses/serializers.py
+++ apps/courses/serializers.py
@@ -84,18 +84,8 @@ class CourseSerializer(serializers.ModelSerializer):
 
     # NESTED RELATIONSHIP - 0-0
     # reviews = ReviewSerializer(many=True, read_only=True)
-
     # HYPERLINK RELATED FIELD - ;-;
     # reviews = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='review-detail')
- 
     # PRIMARYKEY RELATED FIELD - :)
-
-    # media_review = serializers.SerializerMethodField()
-    # def get_media_review(self, obj):
-    #     media = obj.reviews.aggregate(Avg('review')).get('review__avg')
-
-    #     if media is None:
-    #         return 0
-    #     return round(media*2)/2
 
  
